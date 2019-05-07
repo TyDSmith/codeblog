81105afde34c1997f1b7eaa7e05dc73a76079d62
TyDSmith <36959531+TyDSmith@users.noreply.github.com>
2019-05-06 19:37:49 -0700

    begin index and single-post file setups with handlebars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -9,9 +9,20 @@ module.exports = function(app) {
   //       examples: dbExamples
   //     });
   //   });
-  res.send ("hello world");
+  res.render("index");
+
   });
 
+  app.get("/single-post", function(req, res) {
+    //   db.Example.findAll({}).then(function(dbExamples) {
+    //     res.render("index", {
+    //       msg: "Welcome!",
+    //       examples: dbExamples
+    //     });
+    //   });
+    res.render("single-post");
+  
+    });
   // Load example page and pass in an example by id
   // app.get("/example/:id", function(req, res) {
   //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
