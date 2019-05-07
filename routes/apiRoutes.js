ed164cfa85b218934a0c613e70807512a2ae88c3
Nick Ducca <lapplejack6@gmail.com>
2019-05-06 21:31:15 -0700

    commit

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,12 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  // app.get("/api/examples", function(req, res) {
-  //   db.Example.findAll({}).then(function(dbExamples) {
-  //     res.json(dbExamples);
-  //   });
-  // });
+  app.get("/api/blog", function(req, res) {
+    res.json(PORT);
+    // db.get({}).then(function(db) {
+    //   res.json(db);
+    // });
+  });
 
   // Create a new example
   // app.post("/api/examples", function(req, res) {
