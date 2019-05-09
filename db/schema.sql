f068f43545f47558f6b7a6a3180684a29a660e9e
Julian Hicks <jkballin@gmail.com>
2019-05-08 18:58:34 -0700

    sql fixes

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,7 @@ Create Table Blogs (
     id INT Primary Key Auto_Increment,
     title Varchar (100) NOT NULL,
     content Varchar (200) NOT NULL,
-    header_image "" Not NULL ,
+    header_image LONGBLOB Not NULL ,
     author Varchar (200) NOT NULL, 
     submit_date DATE
 );
