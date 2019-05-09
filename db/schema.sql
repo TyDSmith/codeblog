c7ddca02aef4c7b86408994fe396b3fbdb15537c
Julian Hicks <jkballin@gmail.com>
2019-05-08 19:41:20 -0700

    db change

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ Create Database bloggers_db
 Use bloggers_db
 
 Create Table Blogs (
-    id INT Primary Key Auto_Increment,
+    post_id INT Primary Key Auto_Increment,
     title Varchar (100) NOT NULL,
     content Varchar (200) NOT NULL,
     header_image LONGBLOB Not NULL ,
