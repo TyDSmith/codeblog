e21cab02d1eccbf8c4851159a7221631858c38d9
TyDSmith <36959531+TyDSmith@users.noreply.github.com>
2019-05-13 19:07:11 -0700

    fix db credentials

--- server.js
+++ server.js
@@ -22,10 +22,10 @@ var mysql = require('mysql');
 
 var connection = mysql.createConnection({
 
-    host: "localhost",
-    user: "ty",
-    password: "password",
-    database: "test_db_blog"
+    host: "http://v02yrnuhptcod7dk.cbetxkdyhwsb.us-east-1.rds.amazonaws.com/",
+    user: "d8sfxcrhux4qrfh8",
+    password: "na0qh0mw3v80fvu7",
+    database: "pq207a6dbgeya65s"
 
 });
 
